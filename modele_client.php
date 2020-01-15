695e68f116eee8532b655c84313d78a354cd1931
qchastel <qchastel@iut.univ-paris8.fr>
2020-01-15 14:34:39 +0100

    mise a jour header

--- modele_client.php
+++ modele_client.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 		<title>LifeHealh</title>
-		<link href="img/styleVue_client.css" rel="stylesheet" type="text/css">
+		<link href="img/styleModele_client.css" rel="stylesheet" type="text/css">
 
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
@@ -11,23 +11,26 @@
 		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 	</head>
+	
 	<body>
-		<div id="divTitre" >
-			<img id="TitreImg"src="img/Titre.png" alt="Logo du site">
-			<a id="TitreA1" href="index.php?module=connexion" >Connexion   </a>	
-			<a id="TitreA2" href="index.php?module=connexion" >Inscription </a>		
+		
+		<a id="logo"href="model_c"><img src="img/logo.png" alt="Logo du site"></a>
+		<img id="Titre"src="img/Titre.png" alt="Logo du site">
+		<div id="connexion">
+			<a href="modele_choixConnexion.php" >Connexion/
+			Inscription</a>	
 		</div>
 		<header>
 		<nav id="Barre">
-		<a href="index.php">Acceuil</a><!-- Les liens avec des # vise des pages pas encore crée -->
-		<a href="#">Historique des soins</a>
+		<a href="index.php">Accueil</a><!-- Les liens avec des # vise des pages pas encore crée -->
+		<a href="">Historique des soins</a>
 		<a href="#">Antécédents médicaux</a>
 		<a href="#">Résultat examens</a>
 		<a href="#">Compte rendu</a>
 		<a href="#">Coordonnées</a>
 		</nav>
 		</header>
-<!--		<section>
+		<section>
 			<article>
 			<h1>Annonce</h1>
 			<p>Ce site est en cours de dévellopement Ce site est en cours de dévellopement Ce site est en cours de dévellopement Ce site est en cours de dévellopement </p>
@@ -42,6 +45,6 @@
 		<a id="logo"href="model_client.html"><img src="img/logo.png" alt="Logo du site"></a>
 		<p>Life Health, tous droits révervés</p>		
 		<a id="logo"href="model_client.html"><img src="img/logo.png" alt="Logo du site"></a>
-		</footer>-->
+		</footer>
 	</body>
 </html>
